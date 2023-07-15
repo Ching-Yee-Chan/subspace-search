28aff8bec248b58e842c383a849a4a8572a85b95
Ching-Yee-Chan <fkzxy4213@qq.com>
2023-07-15 23:39:26 +0800

    add pytorch IMAE

--- Application/search.py
+++ Application/search.py
@@ -8,7 +8,7 @@ from PyQt5 import QtWidgets
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('dataset', help='desired dataset', choices=['MNIST', 'PGGAN_celebahq', 'GANSynth', 'IMGAN_flight', 'IMGAN_chair'])
+    parser.add_argument('dataset', help='desired dataset', choices=['MNIST', 'PGGAN_celebahq', 'GANSynth', 'IMGAN_flight', 'IMGAN_chair', 'IM-AE'])
     parser.add_argument('--output_path', help='where the saved results would be put', default='.')
     parser.add_argument('--initial', help='intial file')
     parser.add_argument('--use_approx', help='Use stochastic jacobian', default=True)
@@ -67,6 +67,29 @@ if __name__ == "__main__":
         weights_path1 = '../pretrained_weights/IMGAN_chair/03001627_vox_z_128_128/ZGAN.model-10000'
         weights_path2 = '../pretrained_weights/IMGAN_chair/03001627_vox_64/IMAE.model-191'
         model.load_model(weights_path1, weights_path2)
+        title = "IM-GAN"
+    elif dataset == "IM-AE":
+        from SearchUI import OpenGLSearchUI
+        from models.IM_AE import IM_AE
+        # python main.py --ae --sample_dir samples/im_ae_out --start 0 --end 9000 --getz
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--epoch", action="store", dest="epoch", default=0, type=int, help="Epoch to train [0]")
+        parser.add_argument("--iteration", action="store", dest="iteration", default=0, type=int, help="Iteration to train. Either epoch or iteration need to be zero [0]")
+        parser.add_argument("--learning_rate", action="store", dest="learning_rate", default=0.00005, type=float, help="Learning rate for adam [0.00005]")
+        parser.add_argument("--beta1", action="store", dest="beta1", default=0.5, type=float, help="Momentum term of adam [0.5]")
+        parser.add_argument("--dataset", action="store", dest="dataset", default="all_vox256_img", help="The name of dataset")
+        parser.add_argument("--checkpoint_dir", action="store", dest="checkpoint_dir", default="checkpoint", help="Directory name to save the checkpoints [checkpoint]")
+        parser.add_argument("--data_dir", action="store", dest="data_dir", default="../data/all_vol256_img/", help="Root directory of dataset [data]")
+        parser.add_argument("--sample_dir", action="store", dest="sample_dir", default="../samples/im_ae_out", help="Directory name to save the image samples [samples]")
+        parser.add_argument("--sample_vox_size", action="store", dest="sample_vox_size", default=64, type=int, help="Voxel resolution for coarse-to-fine training [64]")
+        parser.add_argument("--train", action="store_true", dest="train", default=False, help="True for training, False for testing [False]")
+        parser.add_argument("--start", action="store", dest="start", default=0, type=int, help="In testing, output shapes [start:end]")
+        parser.add_argument("--end", action="store", dest="end", default=9000, type=int, help="In testing, output shapes [start:end]")
+        parser.add_argument("--ae", action="store_true", dest="ae", default=True, help="True for ae [False]")
+        parser.add_argument("--svr", action="store_true", dest="svr", default=False, help="True for svr [False]")
+        parser.add_argument("--getz", action="store_true", dest="getz", default=True, help="True for getting latent codes [False]")
+        FLAGS = parser.parse_known_args()[0]
+        model = IM_AE(FLAGS)
         title = "IM-GAN"
 
     app = QtWidgets.QApplication(sys.argv)
